07cb6c4d4901532df9ca2da27369d9ba33ed1a49
revxcode <akunpo5521@gmail.com>
2025-04-09 19:08:31 +0700

    Portfolio with Svelte

---
+++ src/pages/about.svelte
@@ -0,0 +1,8 @@
+<section class="mx-auto max-w-4xl mt-20">
+	<div class="flex flex-col justify-center">
+		<div class="mb-4 text-center md:text-start">
+			<h1 class="text-3xl font-bold text-white">About</h1>
+			<p class="mt-2 text-gray-600">This is the about page.</p>
+		</div>
+	</div>
+</section>
